5a0e43717da7d9392ec8f7223fae4dd208f99968
Umar Khan <ukhan@venacorp.com>
2019-07-30 02:02:56 -0400

    Add 'Button' and 'FlatButton' stories

--- src/components/Buttons/Button.stories.js
+++ src/components/Buttons/Button.stories.js
@@ -4,31 +4,128 @@ import { action } from "@storybook/addon-actions";
 
 import Button from "./Button";
 
-export const task = {
-  id: "1",
-  title: "Test Task",
-  state: "TASK_INBOX",
-  updatedAt: new Date(2018, 0, 1, 9, 0)
-};
-
 export const actions = {
-  onPinTask: action("onPinTask"),
-  onArchiveTask: action("onArchiveTask")
+  onClick: action("onClick")
 };
 
 storiesOf("Button", module)
-  .add("default", () => (
-    <Button color="primary" onClick={() => {}}>
-      Primary
-    </Button>
+  .add("Default", () => (
+    <div>
+      <h1>Standard Button</h1>
+      <Button
+        color="primary"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Primary
+      </Button>
+
+      <Button
+        color="secondary"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </Button>
+
+      <Button
+        color="confirmation"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Confirmation
+      </Button>
+
+      <Button
+        color="danger"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Danger
+      </Button>
+    </div>
   ))
-  .add("pinned", () => (
-    <Button color="secondary" onClick={() => {}}>
-      Secondary
-    </Button>
+  .add("Disabled State", () => (
+    <div>
+      <h1>Disabled State</h1>
+      <Button
+        color="primary"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Primary
+      </Button>
+
+      <Button
+        color="secondary"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </Button>
+
+      <Button
+        color="confirmation"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Confirmation
+      </Button>
+
+      <Button
+        color="danger"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Danger
+      </Button>
+    </div>
   ))
-  .add("archived", () => (
-    <Button color="confirmation" onClick={() => {}}>
-      Confirmation
-    </Button>
+  .add("Loading State", () => (
+    <div>
+      <h1>Loading State</h1>
+      <Button
+        color="primary"
+        isLoading={true}
+        iconClass="fa fa-plus"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Create
+      </Button>
+
+      <Button
+        color="secondary"
+        isLoading={true}
+        iconClass="fa fa-cloud-upload"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </Button>
+
+      <Button
+        color="confirmation"
+        isLoading={true}
+        iconClass="fa fa-plus"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Attach
+      </Button>
+
+      <Button
+        color="danger"
+        isLoading={true}
+        iconClass="fa fa-trash"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Delete
+      </Button>
+    </div>
   ));
--- src/components/Buttons/FlatButton.stories.js
+++ src/components/Buttons/FlatButton.stories.js
@@ -4,31 +4,101 @@ import { action } from "@storybook/addon-actions";
 
 import FlatButton from "./FlatButton";
 
-export const task = {
-  id: "1",
-  title: "Test Task",
-  state: "TASK_INBOX",
-  updatedAt: new Date(2018, 0, 1, 9, 0)
-};
-
 export const actions = {
-  onPinTask: action("onPinTask"),
-  onArchiveTask: action("onArchiveTask")
+  onClick: action("onClick")
 };
 
 storiesOf("FlatButton", module)
-  .add("default", () => (
-    <FlatButton color="primary" onClick={() => {}}>
-      Primary
-    </FlatButton>
+  .add("Default", () => (
+    <div>
+      <h1>Standard FlatButton</h1>
+      <FlatButton
+        color="primary"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Primary
+      </FlatButton>
+
+      <FlatButton
+        color="secondary"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </FlatButton>
+
+      <FlatButton
+        color="danger"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Danger
+      </FlatButton>
+    </div>
   ))
-  .add("pinned", () => (
-    <FlatButton color="secondary" onClick={() => {}}>
-      Secondary
-    </FlatButton>
+  .add("Disabled State", () => (
+    <div>
+      <h1>Disabled State</h1>
+      <FlatButton
+        color="primary"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Primary
+      </FlatButton>
+
+      <FlatButton
+        color="secondary"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </FlatButton>
+
+      <FlatButton
+        color="danger"
+        disabled={true}
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Danger
+      </FlatButton>
+    </div>
   ))
-  .add("archived", () => (
-    <FlatButton color="danger" onClick={() => {}}>
-      Danger
-    </FlatButton>
+  .add("Loading State", () => (
+    <div>
+      <h1>Loading State</h1>
+      <FlatButton
+        color="primary"
+        isLoading={true}
+        iconClass="fa fa-plus"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Create
+      </FlatButton>
+
+      <FlatButton
+        color="secondary"
+        isLoading={true}
+        iconClass="fa fa-cloud-upload"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Secondary
+      </FlatButton>
+
+      <FlatButton
+        color="danger"
+        isLoading={true}
+        iconClass="fa fa-trash"
+        style={{ marginRight: "10px" }}
+        onClick={actions.onClick}
+      >
+        Delete
+      </FlatButton>
+    </div>
   ));
