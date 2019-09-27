12d5908253f009abf1d7728500f68c48fc4380d1
Krithika Janakiraman <KJanakiraman@KJanakiraman-Mac.local>
2019-09-27 14:04:32 -0400

    adding dropdown

---
+++ src/components/Dropdown/Dropdown.js
@@ -0,0 +1,217 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { withStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import {
+  BLUE_50,
+  GRAY_50,
+  GRAY_90,
+  GRAY_30,
+  RED_50,
+  WHITE,
+  BLACK
+} from "../../styles/colours";
+
+const styles = theme => {
+  return {
+    container: {
+      display: "flex",
+      flexWrap: "wrap"
+    },
+    textField: {
+      width: 300
+    },
+    listItem: {
+      display: "flex",
+      alignItems: "center",
+      justifyContent: "left",
+      padding: "3px 5px",
+      fontSize: 14
+    },
+    paper: {
+      top: "148 !important",
+      left: "9 !important",
+      boxShadow: "0px 1px 1px " + GRAY_50 + ", 0px 1px 1px " + GRAY_50,
+      borderRadius: 0
+    },
+    select: {
+      border: "solid 1px " + GRAY_50,
+      padding: "2px 20px 2px 8px",
+      fontSize: "14px",
+      height: "32px",
+      display: "flex",
+      justifyContent: "left",
+      alignItems: "center",
+      "&:focus": {
+        background: WHITE,
+        border: "solid 1px " + BLUE_50
+      },
+      "&:hover": {
+        border: "solid 1px " + GRAY_90
+      }
+    },
+    listContainer: {
+      padding: 0,
+      width: 300
+    },
+    inputOnFocus: {
+      marginTop: 8
+    },
+    inputLabel: {
+      fontSize: 18,
+      color: BLACK
+    },
+    placeholder: {
+      color: GRAY_90
+    },
+    disabledSelect: {
+      background: GRAY_30,
+      border: 0,
+      "&:hover": {
+        border: 0
+      },
+      "&:focus": {
+        background: WHITE
+      }
+    },
+    errorSelect: {
+      border: "solid 1px " + RED_50,
+      padding: "2px 20px 2px 8px",
+      fontSize: "14px",
+      height: "28px",
+      display: "flex",
+      justifyContent: "left",
+      alignItems: "center",
+      "&:focus": {
+        background: WHITE
+      }
+    },
+    helperText: {
+      fontStyle: "italic"
+    }
+  };
+};
+
+function Dropdown({
+  classes,
+  placeholder,
+  options,
+  value,
+  helperText,
+  onChange,
+  disabled,
+  error,
+  required
+}) {
+  let dropdownOptions = options;
+  if (placeholder) {
+    dropdownOptions = [
+      {
+        value: placeholder,
+        menuListContent: (
+          <div className={classes.placeholder}>{placeholder}</div>
+        )
+      },
+      ...options
+    ];
+  }
+  return (
+    <form className={classes.container} noValidate autoComplete="off">
+      <TextField
+        id="standard-select-currency"
+        select
+        label="Select Operation"
+        className={classes.textField}
+        disabled={disabled}
+        value={value ? value : dropdownOptions[0].value}
+        disableUnderline={true}
+        onChange={onChange}
+        FormHelperTextProps={{
+          error: error ? true : false,
+          classes: {
+            root: classes.helperText
+          }
+        }}
+        InputProps={{
+          disableUnderline: true
+        }}
+        InputLabelProps={{
+          shrink: true,
+          className: classes.inputLabel,
+          FormLabelClasses: {
+            root: classes.labelRoot,
+            disabled: classes.labelDisabled,
+            error: classes.labelError,
+            focused: classes.labelFocused
+          },
+          required: required
+        }}
+        SelectProps={{
+          classes: {
+            root: classes.inputOnFocus,
+            select: error ? classes.errorSelect : classes.select,
+            disabled: classes.disabledSelect
+          },
+          MenuProps: {
+            className: classes.menu,
+            classes: { paper: classes.paper },
+            MenuListProps: {
+              className: classes.listContainer
+            }
+          }
+        }}
+        helperText={helperText}
+        margin="normal"
+      >
+                
+        {dropdownOptions.map(option => (
+          <MenuItem
+            disableGutters
+            classes={{ root: classes.listItem }}
+            key={option.value}
+            value={option.value}
+          >
+                        {option.menuListContent}       
+          </MenuItem>
+        ))}
+      </TextField>
+    </form>
+  );
+}
+
+Dropdown.defaultProps = {
+  value: "",
+  disabled: false,
+  required: false,
+  helperText: "Some important text"
+};
+
+Dropdown.propTypes = {
+  /**@ignore*/
+  /**Sets the background and text colours.*/
+  classes: PropTypes.object.isRequired,
+  /** Sets the selected option of the dropdown*/
+  value: PropTypes.node,
+  /** Disables the dropdown*/
+  disabled: PropTypes.bool,
+  /** Adds an asterisk beside label */
+  required: PropTypes.bool,
+  /** Sets the helper text */
+  helperText: PropTypes.string,
+  /** These are the options to be displayed in the dropdown.
+   * They are an array of objects, each object contains a value
+   * and a menuListContent property
+   **/
+
+  options: PropTypes.array.isRequired,
+  /** Sets the placeholder inside the dropdown */
+  placeholder: PropTypes.string,
+  /** Callback for when the options in the dropdown are changed */
+  onChange: PropTypes.func,
+  /** Passed as true or false depending on whether the error state needs to be shown */
+  error: PropTypes.bool
+};
+
+export const DropdownComponent = Dropdown;
+export default withStyles(styles)(Dropdown);
---
+++ src/components/Dropdown/Dropdown.stories.js
@@ -0,0 +1,134 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import { withInfo } from "@storybook/addon-info";
+import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
+import DeleteIcon from "@material-ui/icons/Delete";
+import ThreeSixtyIcon from "@material-ui/icons/ThreeSixty";
+
+import Dropdown, { DropdownComponent } from "./Dropdown";
+
+export const actions = {
+  onClick: action("onClick")
+};
+
+const addinTheme = createMuiTheme({
+  venaTheme: "addin"
+});
+
+const webTheme = createMuiTheme({
+  venaTheme: "web"
+});
+
+const dropdownOptions = [
+  {
+    value: "option1",
+    menuListContent: <div>a</div>
+  },
+  {
+    value: "option2",
+    menuListContent: (
+      <>
+                
+        <div style={{ display: "inline-block" }}>
+          <DeleteIcon />
+        </div>
+        <div>Delete this option</div>
+      </>
+    )
+  },
+  {
+    value: "option3",
+    menuListContent: (
+      <>
+                
+        <div style={{ display: "inline-block" }}>
+          <ThreeSixtyIcon />
+        </div>
+        <div
+          style={{
+            display: "inline-block",
+            textOverflow: "ellipsis",
+            overflow: "hidden",
+            paddingLeft: "5px"
+          }}
+        >
+                    This is a very very very long statment that is bigger than the
+                    dropdown menu         
+        </div>
+      </>
+    )
+  }
+];
+
+class DropdownDemo extends React.Component {
+  state = {
+    value: ""
+  };
+
+  onChange = event => {
+    this.setState({ value: event.target.value });
+  };
+
+  render() {
+    return (
+      <Dropdown
+        placeholder={"Select an option"}
+        options={dropdownOptions}
+        value={this.state.value}
+        onChange={this.onChange}
+        required
+      />
+    );
+  }
+}
+
+storiesOf("Dropdown", module)
+  .add(
+    "Default",
+    withInfo({
+      source: false,
+      propTables: [DropdownComponent],
+      propTablesExclude: [Dropdown]
+    })(() => (
+      <div>
+        <MuiThemeProvider theme={webTheme}>
+          <div>
+            <h2>Standard Dropdown</h2>
+            <DropdownDemo />
+          </div>
+        </MuiThemeProvider>
+      </div>
+    ))
+  )
+  .add("Disabled", () => (
+    <div>
+      <MuiThemeProvider theme={webTheme}>
+        <div>
+          <h2>Standard Dropdown</h2>
+          <Dropdown
+            placeholder={"Select an option"}
+            options={dropdownOptions}
+            value={""}
+            disabled
+          />
+        </div>
+      </MuiThemeProvider>
+    </div>
+  ))
+  .add("Error", () => (
+    <div>
+      <MuiThemeProvider theme={webTheme}>
+        <div>
+          <h2>Standard Dropdown</h2>
+          <Dropdown
+            placeholder={"Select an option"}
+            options={dropdownOptions}
+            value={""}
+            error
+            helperText={"This is an error"}
+          />
+        </div>
+      </MuiThemeProvider>
+    </div>
+  ));
