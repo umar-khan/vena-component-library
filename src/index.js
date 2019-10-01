4d7ef78a3a2ab2d5e135e1795c821593352f868f
Umar K <ukhan@venacorp.com>
2019-10-01 15:54:09 -0400

    Add Select Component to Build

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 export { default as Button } from "./components/Buttons/Button";
 export { default as FlatButton } from "./components/Buttons/FlatButton";
+export { default as Select } from "./components/Inputs/Select";
 export { default as Switch } from "./components/Switches/Switch";
 export { default as TextField } from "./components/Inputs/TextField";
