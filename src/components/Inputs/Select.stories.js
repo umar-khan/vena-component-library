e99088c641ee2cfde5464f05d14380cec1631e3e
Umar Khan <ukhan@venacorp.com>
2019-10-07 22:58:23 -0400

    Fix Select Component Styling

--- src/components/Inputs/Select.stories.js
+++ src/components/Inputs/Select.stories.js
@@ -2,15 +2,13 @@ import React from "react";
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
 import { withInfo } from "@storybook/addon-info";
-import { createMuiTheme, MuiThemeProvider } from "@material-ui/core/styles";
-import DeleteIcon from "@material-ui/icons/Delete";
-import ThreeSixtyIcon from "@material-ui/icons/ThreeSixty";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  withStyles
+} from "@material-ui/core/styles";
 
 import Select, { SelectComponent } from "./Select";
-
-export const actions = {
-  onClick: action("onClick")
-};
 
 const addinTheme = createMuiTheme({
   venaTheme: "addin"
@@ -20,41 +18,46 @@ const webTheme = createMuiTheme({
   venaTheme: "web"
 });
 
+const styles = theme => ({
+  selectField: {
+    margin: "16px 10px 8px 0"
+  },
+  customWidth: {
+    margin: "16px 10px 8px 0",
+    width: "400px"
+  },
+  fullWidthField: {
+    margin: "16px 0 8px 0"
+  }
+});
+
 const dropdownOptions = [
   {
     value: "option1",
-    menuListContent: <div>a</div>
+    menuListContent: "First Option"
   },
   {
     value: "option2",
-    menuListContent: (
-      <>
-        <div style={{ display: "inline-block" }}>
-          <DeleteIcon />
-        </div>
-        <div>Delete this option</div>
-      </>
-    )
+    menuListContent: "Second Option"
   },
   {
     value: "option3",
-    menuListContent: (
-      <>
-        <div style={{ display: "inline-block" }}>
-          <ThreeSixtyIcon />
-        </div>
-        <div
-          style={{
-            display: "inline-block",
-            textOverflow: "ellipsis",
-            overflow: "hidden",
-            paddingLeft: "5px"
-          }}
-        >
-          This is a very long statment that is bigger than the dropdown menu
-        </div>
-      </>
-    )
+    menuListContent: "Third Option"
+  }
+];
+
+const longDropDownOptions = [
+  {
+    value: "option1",
+    menuListContent: "First Option that is longer than the standward width"
+  },
+  {
+    value: "option2",
+    menuListContent: "Second Option that is longer than the standward width"
+  },
+  {
+    value: "option3",
+    menuListContent: "Third Option that is longer than the standward width"
   }
 ];
 
@@ -68,69 +71,124 @@ class SelectDemo extends React.Component {
   };
 
   render() {
+    const { classes } = this.props;
+
     return (
-      <Select
-        id={"dropdown_id"}
-        label={"Select Operation"}
-        helperText={"Some important text"}
-        placeholder={"Select an option"}
-        options={dropdownOptions}
-        value={this.state.value}
-        onChange={this.onChange}
-        required
-      />
+      <>
+        <Select
+          id={"dropdown-base"}
+          label={"Select Operation"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+        />
+
+        <Select
+          id={"dropdown-required"}
+          label={"Required"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          required
+        />
+
+        <Select
+          id={"dropdown-helper"}
+          label={"Helper Text"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+          required
+        />
+
+        <Select
+          id={"dropdown-disabled"}
+          label={"Disabled"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+          disabled
+        />
+
+        <Select
+          id={"dropdown-placeholder"}
+          label={"With Placeholder"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+          placeholder="Placeholder"
+        />
+
+        <Select
+          id={"dropdown-error"}
+          label={"Error"}
+          className={classes.selectField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+          error
+        />
+
+        <Select
+          id={"dropdown-long-options"}
+          label={"Long Options"}
+          className={classes.selectField}
+          options={longDropDownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+        />
+
+        <Select
+          id={"dropdown-custom-width"}
+          label={"Custom Width"}
+          className={classes.customWidth}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+        />
+
+        <Select
+          id={"dropdown-full-width"}
+          label={"Full Width"}
+          className={classes.fullWidthField}
+          options={dropdownOptions}
+          value={this.state.value}
+          onChange={this.onChange}
+          helperText={"Some important text"}
+          fullWidth
+        />
+      </>
     );
   }
 }
 
-storiesOf("Select", module)
-  .add(
-    "Default",
-    withInfo({
-      source: false,
-      propTables: [SelectComponent],
-      propTablesExclude: [Select, SelectDemo, MuiThemeProvider]
-    })(() => (
-      <div>
-        <MuiThemeProvider theme={addinTheme}>
-          <div>
-            <h2>Standard Select</h2>
-            <SelectDemo />
-          </div>
-        </MuiThemeProvider>
-      </div>
-    ))
-  )
-  .add("Disabled", () => (
+const StyledDemo = withStyles(styles)(SelectDemo);
+
+storiesOf("Select", module).add(
+  "Default",
+  withInfo({
+    source: false,
+    propTables: [SelectComponent],
+    propTablesExclude: [Select, SelectDemo, MuiThemeProvider]
+  })(() => (
     <div>
       <MuiThemeProvider theme={addinTheme}>
         <div>
-          <h2>Disabled Select</h2>
-          <Select
-            placeholder={"Select an option"}
-            label={"Select Operation"}
-            options={dropdownOptions}
-            value={""}
-            disabled
-          />
+          <h2>Standard Select</h2>
+          <StyledDemo />
         </div>
       </MuiThemeProvider>
     </div>
   ))
-  .add("Error", () => (
-    <div>
-      <MuiThemeProvider theme={addinTheme}>
-        <div>
-          <h2>Error Select</h2>
-          <Select
-            placeholder={"Select an option"}
-            label={"Select Operation"}
-            options={dropdownOptions}
-            value={""}
-            error
-            helperText={"This is an error"}
-          />
-        </div>
-      </MuiThemeProvider>
-    </div>
-  ));
+);
