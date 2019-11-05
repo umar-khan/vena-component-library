d3ade72137e7a0639847731606722188cfca7144
absheikh <absheikh@venacorp.com>
2019-11-05 15:59:07 -0500

    Added accessibility addon

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,4 +1,7 @@
-import { configure } from "@storybook/react";
+import { configure, addDecorator } from "@storybook/react";
+import { withA11y } from '@storybook/addon-a11y';
+
+addDecorator(withA11y);
 
 // automatically import all files ending in *.stories.js
 const req = require.context("../src", true, /\.stories\.js$/);
